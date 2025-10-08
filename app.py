cb6ed4668c407b42a42d64cbc3fa9d713bc05d71
Thiago Prochnow Borges <thiagoprochnow1@gmail.com>
2025-10-07 21:42:31 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -61,8 +61,10 @@ def formatar_numero(value, precision=1):
 # 3. Sidebar and filters (Users inputs)
 # ==============================================================================
 
+st.sidebar.markdown(f"🔎 {len(df['country'].unique())} countries analyzed")
+
 #Create the header for the filters
-st.sidebar.header("Filters")
+st.sidebar.header("📌 Filters",divider="grey")
 
 # --- CONTINENT FILTER ---
 # Create the list of unique continents and sort
@@ -75,20 +77,9 @@ continent_filter = st.sidebar.selectbox(
 )
 # Condition for global selector, because was not original in dataset
 if continent_filter == 'Global':
-    df_base = df.copy()
+    df_continent = df.copy()
 else:
-    df_base = df[df['Continent'] == continent_filter]
-
-# --- COUNTRY FILTER ---
-# Create the list of unique countries and sort
-country_list = sorted(df_base['country'].unique())
-# Added "Global" option to the top of list
-options_selectbox_country = ['Global'] + country_list
-# Create country selector
-country_filter = st.sidebar.selectbox(
-    label="Select country",
-    options=options_selectbox_country
-)
+    df_continent = df[df['Continent'] == continent_filter]
 
 # --- YEAR FILTER ---
 # Create year slider
@@ -97,7 +88,6 @@ selected_year = st.sidebar.slider(
     min_value=int(df['year'].min()),
     max_value=int(df['year'].max())
 )
-
 
 st.sidebar.markdown("---")
 
@@ -120,107 +110,57 @@ st.sidebar.markdown(
 # ==============================================================================
 
 # --- Ranking calculations (related only for year) ---
-df_filter_year = df_base[df_base['year'] == selected_year]
-top_5_best = df_filter_year.sort_values(by="prosperity_score", ascending=False).head(5)
-top_5_worst = df_filter_year.sort_values(by="prosperity_score", ascending=True).head(5)
+df_filter_year = df_continent[df_continent['year'] == selected_year]
 
-# Set dataframe for Global option
-if country_filter == 'Global':
-    df_filtrado_raw = df_base.groupby('year').agg(
-        population=('population', 'sum'),
-        prosperity_score=('prosperity_score', 'mean'),
-        per_capita=('per capita', 'mean'),
-        imports_of_goods_and_services=('imports of goods and services', 'sum'),
-        exports_of_goods_and_services=('exports of goods and services', 'sum')
-    ).reset_index()
-
-else:
-    # Raw/original dataframe
-    df_filtrado_raw = df_base[df_base['country'] == country_filter]
-
-# Variables for current and previous date
-data_current_year = df_filtrado_raw[df_filtrado_raw['year'] == selected_year]
-data_previous_year = df_filtrado_raw[df_filtrado_raw['year'] == selected_year - 1]
-
-# Avoid errors if it does not exist
-score_current, pc_current, pop_current, export_current, imports_current, trade_balance = 0, 0, 0, 0, 0, 0
-delta_score, delta_pc, delta_pop, delta_export, delta_imports, delta_trade_balance = 0, 0, 0, 0, 0, 0
-
-
-if not data_current_year.empty:
-    # Columns was renamed in the aggregation, so we need to check both possible names
-    pc_col_name = 'per_capita' if 'per_capita' in data_current_year.columns else 'per capita'
-    imports_col_name = 'imports_of_goods_and_services' if 'imports_of_goods_and_services' in data_current_year.columns else 'imports of goods and services'
-    exports_col_name = 'exports_of_goods_and_services' if 'exports_of_goods_and_services' in data_current_year.columns else 'exports of goods and services'
-
-    # Get the values of current year
-    score_current = data_current_year['prosperity_score'].iloc[0]
-    pc_current = data_current_year[pc_col_name].iloc[0]
-    pop_current = data_current_year['population'].iloc[0]
-    export_current = data_current_year[exports_col_name ].iloc[0]
-    imports_current = data_current_year[imports_col_name].iloc[0]
-    trade_balance = export_current - imports_current
-
-if not data_previous_year.empty:
-    # Columns was renamed in the aggregation, so we need to check both possible names
-    pc_col_name_pre = 'per_capita' if 'per_capita' in data_previous_year.columns else 'per capita'
-    imports_col_name_pre = 'imports_of_goods_and_services' if 'imports_of_goods_and_services' in data_previous_year.columns else 'imports of goods and services'
-    exports_col_name_pre = 'exports_of_goods_and_services' if 'exports_of_goods_and_services' in data_previous_year.columns else 'exports of goods and services'
-    
-
-    # Get the values of previous year
-    score_previous = data_previous_year['prosperity_score'].iloc[0]
-    pc_previous = data_previous_year[pc_col_name_pre].iloc[0]
-    pop_previous = data_previous_year['population'].iloc[0]
-    export_previous = data_previous_year[exports_col_name_pre].iloc[0]
-    imports_previous = data_previous_year[imports_col_name_pre].iloc[0]
-    
-    # Calculation of deltas
-    delta_score = score_current - score_previous
-    delta_pc = pc_current - pc_previous
-    delta_pop = pop_current - pop_previous
-    delta_export = export_current - export_previous
-    delta_import = imports_current - imports_previous
-    delta_trade_balance = trade_balance - (export_previous - imports_previous)
+resumos_decadas = {
+    1970: "Marked by the Oil Crisis and the stagflation in Western economies, with the growth of exporting powers as a Japan.",
+    1980: "The decade of cold war late, debt crisis into South America and the beginning of economy opening of China.",
+    1990: "Defined by globalization post-cold war , the boom of tecnology (bubble 'dot com') and the reestructure of economies in the east European.",
+    2000: "Started with the explosion of the internet bubble, follow by global strong growth driven by commodities and by rise of China. Finished with financial crisis of 2008.",
+    2010: "The decade of slow recovery post-2008, rise of social media, comercial tensions between USA and China, and the beginning of new era of technological globalization.",
+    2020: "Started by COVID-19 pandemic, that causes a disruption unprecedented in suply chains and global economies."
+}
 
 
 # ==============================================================================
 # 5. Layout and visual dashboard (Front-end)
 # ==============================================================================
 
-coltitle,coltreemap,colmap  = st.columns(3)
+coltitle,colmap  = st.columns(2)
 
 with coltitle:
     st.title("🌎 Anatomy of Global Prosperity")
-    st.markdown(f"{len(df_base['country'].unique())} countries analyzed")
+    # --- Session of history context ---
+    # Get the year selected (ex: 1985) and transform this into a decade (1980)
+    select_decade = (selected_year // 10) * 10 
 
-with coltreemap:
-    df_treemap = df[df['year'] == selected_year]
-    data_graphic = df_treemap.groupby('Continent')['prosperity_score'].mean().reset_index()
-
-    # Create treemap graphic
-    fig_treemap = px.treemap(
-        data_graphic,
-        path=[px.Constant("Continent"), 'Continent'],
-        values='prosperity_score',
-        color='prosperity_score',
-        color_continuous_scale='Blues',
-        hover_data={'prosperity_score': ':.3f'},
-        title="Proportion of Average Prosperity Score (Continents)"
-    )
-    fig_treemap.update_layout(height=250, margin=dict(t=20, b=10, l=10, r=10))
-    st.plotly_chart(fig_treemap)
+    # Show the summary of decade
+if select_decade in resumos_decadas:
+    st.write(f"**📚 Context of the decade {select_decade}:** {resumos_decadas[select_decade]}")
+st.markdown("---")
 
 with colmap:
     fig_map = px.choropleth(
-        data_graphic,
-        locations='Continent',
+        data_frame=df_filter_year,
+        locations='country',
+        locationmode='country names',
         color='prosperity_score',
         color_continuous_scale='Blues',
+        projection='natural earth',
+        hover_name='country',
+        hover_data={'prosperity_score': ':.3f', 'population': ':.0f', 'per capita': ':.2f'}
     )
-    st.plotly_chart(fig_map)
+    fig_map.update_geos(
+    lataxis_range=[-90, 90], # Limita o arrasto vertical
+    lonaxis_range=[-180, 180] # Limita o arrasto horizontal
+    )
+    fig_map.update_layout(
+        height=300,
+        margin={"r":0,"t":5,"l":0,"b":0}
+        )
+        
+st.plotly_chart(fig_map, use_container_width=True)
 
-st.markdown("---")
 
 # ==============================================================================
 # 5. Create tabs
@@ -228,7 +168,11 @@ st.markdown("---")
 tab1, tab2 = st.tabs(["Overview & Rankings", "Individual Analysis"])
 
 with tab1:
+
     st.header(f"Rankings of prosperity for year {selected_year}", divider="grey")
+
+    top_5_best = df_filter_year.sort_values(by="prosperity_score", ascending=False).head(5)
+    top_5_worst = df_filter_year.sort_values(by="prosperity_score", ascending=True).head(5)
 
     col1, col2 = st.columns(2)
 
@@ -240,9 +184,95 @@ with tab1:
         st.subheader("📉 Top 5 countries least prosperity score")
         st.dataframe(top_5_worst[['country', 'prosperity_score']])
 
+    st.info(
+            """
+            **About the prosperity_score**
+
+            This score is calculated based on the median of several key indicators:
+            * Gross capital formation
+            * Exports of goods and services
+            * Imports of goods and services
+            * Household consumption expenditure
+            * Agriculture, hunting, forestry, fishing
+            * Mining, manufacturing, utilities
+            """
+        )
+
 
 with tab2:
     st.header("Individual Analysis", divider="grey")
+
+    # --- COUNTRY FILTER ---
+    # Create the list of unique countries and sort
+    country_list = sorted(df_continent['country'].unique())
+    # Added "Global" option to the top of list
+    options_selectbox_country = ['Global'] + country_list
+
+    # Create country selector
+    country_filter = st.selectbox(
+        label="Select country",
+        options=options_selectbox_country
+    )
+
+    # Set dataframe for Global option
+    if country_filter == 'Global':
+        df_filtrado_raw = df_continent.groupby('year').agg(
+            population=('population', 'sum'),
+            prosperity_score=('prosperity_score', 'mean'),
+            per_capita=('per capita', 'mean'),
+            imports_of_goods_and_services=('imports of goods and services', 'sum'),
+            exports_of_goods_and_services=('exports of goods and services', 'sum')
+        ).reset_index()
+
+    else:
+        # Raw/original dataframe
+        df_filtrado_raw = df_continent[df_continent['country'] == country_filter]
+
+    # Variables for current and previous date
+    data_current_year = df_filtrado_raw[df_filtrado_raw['year'] == selected_year]
+    data_previous_year = df_filtrado_raw[df_filtrado_raw['year'] == selected_year - 1]
+
+    # Avoid errors if it does not exist
+    score_current, pc_current, pop_current, export_current, imports_current, trade_balance = 0, 0, 0, 0, 0, 0
+    delta_score, delta_pc, delta_pop, delta_export, delta_imports, delta_trade_balance = 0, 0, 0, 0, 0, 0
+
+
+    if not data_current_year.empty:
+        # Columns was renamed in the aggregation, so we need to check both possible names
+        pc_col_name = 'per_capita' if 'per_capita' in data_current_year.columns else 'per capita'
+        imports_col_name = 'imports_of_goods_and_services' if 'imports_of_goods_and_services' in data_current_year.columns else 'imports of goods and services'
+        exports_col_name = 'exports_of_goods_and_services' if 'exports_of_goods_and_services' in data_current_year.columns else 'exports of goods and services'
+
+        # Get the values of current year
+        score_current = data_current_year['prosperity_score'].iloc[0]
+        pc_current = data_current_year[pc_col_name].iloc[0]
+        pop_current = data_current_year['population'].iloc[0]
+        export_current = data_current_year[exports_col_name ].iloc[0]
+        imports_current = data_current_year[imports_col_name].iloc[0]
+        trade_balance = export_current - imports_current
+
+    if not data_previous_year.empty:
+        # Columns was renamed in the aggregation, so we need to check both possible names
+        pc_col_name_pre = 'per_capita' if 'per_capita' in data_previous_year.columns else 'per capita'
+        imports_col_name_pre = 'imports_of_goods_and_services' if 'imports_of_goods_and_services' in data_previous_year.columns else 'imports of goods and services'
+        exports_col_name_pre = 'exports_of_goods_and_services' if 'exports_of_goods_and_services' in data_previous_year.columns else 'exports of goods and services'
+        
+
+        # Get the values of previous year
+        score_previous = data_previous_year['prosperity_score'].iloc[0]
+        pc_previous = data_previous_year[pc_col_name_pre].iloc[0]
+        pop_previous = data_previous_year['population'].iloc[0]
+        export_previous = data_previous_year[exports_col_name_pre].iloc[0]
+        imports_previous = data_previous_year[imports_col_name_pre].iloc[0]
+        
+        # Calculation of deltas
+        delta_score = score_current - score_previous
+        delta_pc = pc_current - pc_previous
+        delta_pop = pop_current - pop_previous
+        delta_export = export_current - export_previous
+        delta_import = imports_current - imports_previous
+        delta_trade_balance = trade_balance - (export_previous - imports_previous)
+
 
     if not data_current_year.empty:
         
@@ -278,25 +308,27 @@ with tab2:
             )
 
 
-        # --- Line graph and raw data ---
-        st.subheader("Historical Data")
 
-    
-        if country_filter == 'Global':
-            y = ["population", "per_capita", "prosperity_score"]
-        else:
-            y = ["population", "per capita", "prosperity_score"]
+        # --- Line graph and raw data---
+        st.subheader("Evolution of indicators")
 
-        st.area_chart(
-            data=df_filtrado_raw.set_index('year'),
-            y= y,
-            use_container_width=True
-            )
+        # --- Logic for create the index data---
+        df_index = df_filtrado_raw[['year', 'population', 'per_capita', 'prosperity_score']].copy()
 
+        # get the first year of data
+        primeiro_ano_df = df_index.iloc[0]
+
+        # Loop for divide each column by her value in year
+        for col in ['population', 'per_capita', 'prosperity_score']:
+            df_index.loc[:, col] = (df_index[col] / primeiro_ano_df[col]) * 1
+
+        # --- Displays the Indexed graphic ---
+        st.line_chart(data=df_index.set_index('year'))
+
+        # Below the graph, the dataframe was set in an expander
         with st.expander("Click to view raw data for the selected year"):
             if country_filter == 'Global':
                 st.dataframe(df_filtrado_raw)
                 
             else:
                 st.dataframe(df_filtrado_raw.iloc[:,:-14])
-
